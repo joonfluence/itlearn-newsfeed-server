b0de515cef256f4517ea5500a6764bc31b128ab2
joonho lee <joonfluence.dev@gmail.com>
2024-08-17 21:17:52 +0900

    Initial Setup

---
+++ src/main/java/com/newsfeed/itlearn/ItlearnApplication.java
@@ -0,0 +1,13 @@
+package com.newsfeed.itlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItlearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItlearnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/newsfeed/itlearn/ItlearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newsfeed.itlearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItlearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
